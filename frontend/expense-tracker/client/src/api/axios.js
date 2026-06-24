8d3e813b15d81aa1950c5143652844d1c14774fb
JustJide1 <aladegbolahan2005@gmail.com>
2026-06-25 00:05:57 +0100

    Fix: use VITE_API_URL env var for backend URL

--- frontend/expense-tracker/client/src/api/axios.js
+++ frontend/expense-tracker/client/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 import useAuthStore from "../store/authStore";
 
-const instance = axios.create({ baseURL: "http://localhost:5000/api" });
+const instance = axios.create({ baseURL: import.meta.env.VITE_API_URL || "http://localhost:5000/api" });
 
 instance.interceptors.request.use((config) => {
     const token = localStorage.getItem("token");
@@ -22,3 +22,4 @@ instance.interceptors.response.use(
 );
 
 export default instance;
+
